e7fb9010559287b0d66e4aeb214774d75cd65eb3
Raphael Floresca <rpngfl@gmail.com>
2023-06-08 17:20:54 +0800

    Add commit message

---
+++ setup.sh
@@ -0,0 +1,19 @@
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl-doc
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+exec "$SHELL"
+
+pyenv install 3.11.3
+
+pyenv global 3.11.3
+
+pyenv virtualenv 3.11.3 cdb-data-internship
+
+pyenv local cdb-data-internship
